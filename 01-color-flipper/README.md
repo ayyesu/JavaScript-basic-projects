39ceeca0b476b9a190fc46dda3b65b6e315f56ca
Daniel Kissiedu <iamdanielkissiedu@gmail.com>
2022-10-21 02:00:51 +0000

    Complete files

---
+++ 01-color-flipper/README.md
@@ -0,0 +1,8 @@
+# About Color Flipper project
+
+This project was built with html,css and JS.<br>
+It contains a `Click me button` which when clicked generates a random color which is displayed on the body of the page.
+
+#### Preview of project
+
+file:///home/travis/Videos/Screencasts/Screencast%20from%2010-21-2022%2001:54:52%20AM.webm
--- 01-color-flipper/hex.js
+++ 01-color-flipper/hex.js
@@ -1,1 +1,18 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function(){
+    let result = "#";
+    let i;
+    for (i=0;i<6;i++){
+        result += hex[randomPick()];
+    }
+    document.body.style.backgroundColor = result;
+    color.textContent = result;
+})
+
+function randomPick(){
+    const pick = Math.floor(Math.random() * hex.length);
+    return pick;
+}
